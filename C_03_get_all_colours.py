5b6ae76438fe4ae0892319deaaa19a3cc777a109
WineDarkside <101750632+WineDarkside@users.noreply.github.com>
2025-02-25 12:02:58 +1300

    added code

--- C_03_get_all_colours.py
+++ C_03_get_all_colours.py
@@ -1,6 +1,19 @@
 import csv
 import random
 
+
+def round_ans(val):
+    """
+    rounds numbers to nearest integer
+    :param val: number to be rounded
+    :RETURN: rounded number (an integer)
+    """
+
+    var_rounded = (val * 2 + 1) // 2
+    raw_rounded = "{:.0f}".format(var_rounded)
+    return int(raw_rounded)
+
+
 # Retrieve colors from csv file and put them in a list
 file = open("00_colour_list_hex_v3.csv", "r")
 all_colors = list(csv.reader(file, delimiter=","))
@@ -8,4 +21,26 @@ file.close()
 
 # remove the first row
 all_colors.pop(0)
-print(all_colors)
+
+round_colours = []
+colour_scores = []
+
+# loop until we have four colours with different scores...
+while len(round_colours) < 4:
+    potential_colours = random.choice(all_colors)
+
+    # get teh score and check it's not a duplicate
+    if potential_colours[1] not in colour_scores:
+        round_colours.append(potential_colours)
+        colour_scores.append(potential_colours[1])
+
+print(round_colours)
+print(colour_scores)
+
+# find target score (median)
+int_scores = [int(x) for x in colour_scores]
+int_scores.sort()
+
+median = (int_scores[1] + int_scores[2]) / 2
+median = round_ans(median)
+print("median", median)
